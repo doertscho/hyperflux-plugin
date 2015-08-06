0f753ada1b1a338c97a7164d0051992ccd9f3680
Stefan Röpstorff <stefan@roepstorff.eu>
2015-08-06 14:49:27 +0200

    A lot of changes I forgot to commit. Rewrites are almost working now.

---
+++ src/main/scala/hyperflux/plugin/HyperfluxInterfaceAnalyzerComponent.scala
@@ -0,0 +1,100 @@
+package hyperflux.plugin
+
+import scala.tools.nsc
+import scala.tools.nsc._
+import scala.tools.nsc.plugins._
+import hyperflux.annotation._
+import scala.collection.mutable.HashMap
+import scala.reflect.internal.Flags._
+
+abstract class HyperfluxInterfaceAnalyzerComponent extends PluginComponent {
+
+  import global._
+
+  val phaseName = "hf-i-analyzer"
+  
+  val hf: HyperfluxStorage[Symbol, Template, DefDef, RefTree, Tree]
+
+  override def newPhase(prev: nsc.Phase): StdPhase =
+    new HyperfluxInterfaceAnalyzerPhase(prev)
+
+  class HyperfluxInterfaceAnalyzerPhase(prev: nsc.Phase) extends StdPhase(prev) {
+    override def name = phaseName
+    override def description =
+      "gathers information and tells client from server parts"
+    
+    override def apply(unit: CompilationUnit) {
+      println("actual unit root symbol: " + unit.body.symbol)
+      findAnnotations(unit.body)
+    }
+    
+    // TODO: find a way to get the class names type-safely at compile time
+    val CLIENT_CLASS_NAME = "hyperflux.annotation.Client"
+    val SERVER_CLASS_NAME = "hyperflux.annotation.Server"
+    var currentPackageID: RefTree = _
+    def findAnnotations(tree: Tree) {
+      tree match {
+        case PackageDef(pid, stats) => {
+          currentPackageID = pid
+          stats foreach findAnnotations
+        }
+        case ModuleDef(_, _, impl) => {
+          tree.symbol.annotations foreach {
+            _.atp.baseClasses foreach {
+              _.fullName match {
+                case CLIENT_CLASS_NAME =>
+                  hf.packageIDs += ((tree.symbol.safeOwner, currentPackageID))
+                  hf.clientObjects += impl.symbol.safeOwner
+                case SERVER_CLASS_NAME => {
+                  hf.packageIDs += ((tree.symbol.safeOwner, currentPackageID))
+                  hf.serverObjects += impl.symbol.safeOwner
+                  analyzeServerObject(impl)
+                }
+                case _ =>
+              }
+            }
+          }
+          // for now, we only allow those annotations on top-level objects
+          //impl.children foreach findAnnotations
+        }
+        case Import(_, _) => // imports can be skipped, no need to go deeper
+        // TODO: optimizations for several node types
+        case _ => //tree.children foreach findAnnotations
+      }
+    }
+    
+    /*
+    def debugTree(tree: Tree) {
+      println("__ debug info")
+      
+      def debugInt(tree: Tree, lvl: Int) {
+        println("  " * lvl + tree.symbol)
+        tree.children foreach { debugInt(_, lvl + 1) }
+      }
+      debugInt(tree, 0)
+    }*/
+    
+    def analyzeServerObject(impl: Template) {
+      hf.serverMethods +=
+        ((impl.symbol.safeOwner, new HashMap[Symbol, DefDef]))
+      // for the sake of simplicity (and in order to enforce a clear design)
+      // only top-level method definitions in our server object are registered
+      // as remotely callable functions
+      impl.children foreach {
+        case d @ DefDef(mods, name, _, _, _, _)
+        if (
+          // private methods should of course not be exposed
+          !(mods.hasAccessBoundary) &&
+          !(mods.hasFlag(PRIVATE)) &&
+          // same for the constructor
+          !(name containsName "<init>") &&
+          // furthermore, we want to exclude methods that are mere accessors to
+          // values and variables (this may be integrated later)
+          !(mods.hasFlag(ACCESSOR))
+        ) =>
+          hf.serverMethods(impl.symbol.safeOwner) += ((d.symbol, d))
+        case _ =>
+      }
+    }
+  }
+}
--- src/main/scala/hyperflux/plugin/HyperfluxPlugin.scala
+++ src/main/scala/hyperflux/plugin/HyperfluxPlugin.scala
@@ -2,19 +2,51 @@ package hyperflux.plugin
 
 import scala.tools.nsc._
 import scala.tools.nsc.plugins._
+import scala.collection.mutable.TreeSet
 
 class HyperfluxPlugin(val global: Global) extends Plugin {
   import global._
 
   val name = "hyperflux"
   val description = "Creates client-server structure"
+
+  // some storage objects that are used across the phases
+  val hf: HyperfluxStorage[Symbol, Template, DefDef, RefTree, Tree] = 
+    new HyperfluxStorage[Symbol, Template, DefDef, RefTree, Tree]
+  
   val components = List[PluginComponent](
-    HyperfluxAnalyzerComponent
+    HyperfluxInterfaceAnalyzerComponent,
+    HyperfluxUsageAnalyzerComponent,
+    HyperfluxProxifierComponent
   )
 
-  object HyperfluxAnalyzerComponent extends {
+  // the analysis part has to be done in two phases because there might well
+  // be references across compilation units, so before looking out for usages
+  // of server methods, we first need to collect the available server methods
+  // from ALL units
+  
+  object HyperfluxInterfaceAnalyzerComponent extends {
     val global: HyperfluxPlugin.this.global.type = HyperfluxPlugin.this.global
     override val runsAfter = List("typer")
+    override val runsRightAfter = Some("typer")
+    override val runsBefore = List("hf-u-analyer")
+    override val hf = HyperfluxPlugin.this.hf
+  } with HyperfluxInterfaceAnalyzerComponent
+
+  object HyperfluxUsageAnalyzerComponent extends {
+    val global: HyperfluxPlugin.this.global.type = HyperfluxPlugin.this.global
+    override val runsAfter = List("hf-i-analyzer")
+    override val runsRightAfter = Some("hf-i-analyzer")
+    override val runsBefore = List("hf-proxyfier")
+    override val hf = HyperfluxPlugin.this.hf
+  } with HyperfluxUsageAnalyzerComponent
+
+  object HyperfluxProxifierComponent extends {
+    val global: HyperfluxPlugin.this.global.type = HyperfluxPlugin.this.global
+    override val runsAfter = List("hf-u-analyzer")
+    override val runsRightAfter = Some("hf-u-analyzer")
     override val runsBefore = List("jsinterop")
-  } with HyperfluxAnalyzerComponent
+    override val hf = HyperfluxPlugin.this.hf
+  } with HyperfluxProxifierComponent
+  
 }
